8fd03f8035de1a706bf3e5e539e0349287890335
Jose Maria Paez <jmpsok@gmail.com>
2022-01-20 19:01:58 +0100

    subiendo seed

--- database/seeders/ReservasTableSeeder.php
+++ database/seeders/ReservasTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App/Reserva;
 
 class ReservasTableSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class ReservasTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i=0; $i < 20; $i++) {
+            Reserva::create([
+                   'nombre' => str_random(10),
+                   'numeroReserva' => str_random(10),
+                   'agencia' => str_random(25),
+                   'alojamiento' =>str_random(10)
+               ]);
+            }
     }
 }
