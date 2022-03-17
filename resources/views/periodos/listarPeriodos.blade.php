860004dc26d3ad54c4dde04abd482d40d2b9f47f
Jose Maria Paez <jmpsok@gmail.com>
2022-03-17 18:13:06 +0100

    creamos carpeta periodos

--- resources/views/periodos/listarPeriodos.blade.php
+++ resources/views/periodos/listarPeriodos.blade.php
@@ -26,7 +26,7 @@
             </tr>
             </thead>
             <tbody>
-              @foreach ($periodos as $periodo)
+              {{-- @foreach ($periodos as $periodo) --}}
             <tr>
                 <td>{{ $periodo -> id }}</td>
                 <td>{{ $periodo -> fechaInicio }}</td>
@@ -36,81 +36,33 @@
                 
                 
                 <td> 
-                <a href="{{ route('articulos.show', $articulo->id)}}">
-                  {{ $articulo -> nombre }}
+                <a href="{{ route('periodos.show', $periodo->id)}}">
+                  {{ $periodo -> nombre }}
                   </a>
                 </td> 
                 <td>
                 <!-- aqui ponemos boton de borrar por post ***** AZUL *****-->
                 <td>
                     <div class="form-group">
-                        <form action="{{ route('periodos.destroy',$articulo->id)}}" method="post">
+                        <form action="{{ route('periodos.destroy',$periodo->id)}}" method="post">
                         @method ('DELETE')
                         @csrf
                         <button type="submit" class="btn btn-primary">BORRAR</button>
                         </form>
                     </div>
                 </td>
-                <!-- Button trigger modal  1 ****** ROJO ********-->
-                <td>            
-                <button type="button" class="btn btn-danger" id="btn_confirm" data-toggle="modal" data-target="#delete_modal_{{$articulo->id}}">
-                Borrar con modal
-                </button>
-                </td>
-                <td>
-                
-                <!-- formulario para la eliminacion -->
-                <form id="delete_form" action="{{ route('periodos.destroy',$articulo->id)}}" method="post">
-                @method ('DELETE')
-                @csrf
                 </form>
-                <!-- Modal ROJO ******* MODAL PARA ELIMINACION **************************-->
-                <div class="modal fade" id="delete_modal_{{$periodo->id}}" tabindex="-1" role="dialog">   >
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Borrado de periodo</h5>
-                            <button type="button" class="close" data-dismiss="modal" arial-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                            <div class="modal-body">
-                            <p>Vas a eliminar el periodo <span id="zona_de_nombre">{{$periodo->id}}</span>
-                            </p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancelar</button>
-                            <form action="{{ route('periodos.destroy',$periodo->id)}}" method="post">
-                            @method ('DELETE')
-                            @csrf
-                            <button type="submit" class="btn btn-danger">BORRAR</button>
-                            </form>
                         </div>
                     </div>
                   </div>
                 </div>
                 </td>
-                <!-- fin del modal ******************* -->
-                <!-- Button trigger modal  2 ****** GRIS ********-->
-                <td>            
-                <button type="button" class="btn btn-secondary" id="btn_confirm2" data-toggle="modal" data-target="#delete_modal2_{{$periodo->id}}">
-                Borrar con modal jquery
-                </button>
-                </td>
-            
-                <!-- ********** Button trigger EDITAR  ************* -->
-                <td>
-                <!-- formulario para la editar -->
-                <div class="form-group">
-                <a href="{{ route('periodos.edit',$periodo->id)}}" class="btn btn-warning" tabindex="-1" role="button" aria-disabled="true">Editar</a>
-                        
-                </div>
-                </td>
+              
                
             
             
             </tr>
-                @endforeach
+                {{-- @endforeach --}}
                 </tbody>
                 </h5>
                 </table>
